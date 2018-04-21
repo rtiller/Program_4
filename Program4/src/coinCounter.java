ee4cb8a677d853a5b141fb3ffd0b7c40d976c188
rtiller <33472584+rtiller@users.noreply.github.com>
2018-04-21 10:32:04 -0600

    Update with new method created for Dynamic program
    created a method for the dynamic program and reinserted main to test it

--- Program4/src/coinCounter.java
+++ Program4/src/coinCounter.java
@@ -8,17 +8,24 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class coinCounter{
-   /*   public static void main(String[] args) {
-        int[] test;
-        int[] sendin = {25,10,5,1};
-         test = coinCounter.coinCounter(sendin, 73);
+      public static void main(String[] args) {
+        int value = 63;
+        int[] coins = {1,5,10,21,25};
+        int[] coinsUsed = new int[value+1];
+        int[] coinsCount = new int[value+1];
         
-//          for (int i = 0; i < test.length; i++) {
-//               System.out.println(test[i]);
-//          }
-//         
+         System.out.print("Making change for " + value + " requires \n");
+         System.out.println( makeChange(coins, value, coinsCount, coinsUsed) + " coins");
+         System.out.print("They are, ");
+         Print(coinsUsed, value);
+         System.out.println("The used list is as follows");
+         printArray(coinsUsed);
+         
+         
+        
+         
       }
-*/
+
     
     
     public static int[] coinCounter(int[] currencyIn, int value)
@@ -47,4 +54,52 @@ public class coinCounter{
         }
         return currencyReturn;
     }
+    
+    public static int  makeChange(int[] currencyIn, int value, int[] minCoins, int[] coinUsed)
+    {
+      
+        for(int cents = 1; cents<value+1; cents++)
+        {
+            int coinCount = cents;
+            int newCoin =1;
+            for(int j = 0; j <currencyIn.length ; j++)
+            {
+                
+                if(currencyIn[j] <= cents)
+                {
+                    if(minCoins[cents-currencyIn[j]]+1 < coinCount)
+                    {
+                        coinCount = minCoins[cents-currencyIn[j]]+1;
+                        newCoin = currencyIn[j];
+                    }
+                }
+            }
+            minCoins[cents] = coinCount;
+            coinUsed[cents] = newCoin;
+    }
+        return minCoins[value];
+    }
+    
+    public static void Print(int[] coinUsed,int value)
+    {
+        int coin = value;
+        while(coin > 0)
+        {
+            int thisCoin = coinUsed[coin];
+            System.out.print(thisCoin + " ");
+            coin = coin - thisCoin;
+        }
+    }
+    
+    public static void printArray(int[] coinUsed)
+    {
+        System.out.print("[");
+        for(int x = 1; x < coinUsed.length; x++)
+        {
+            int thisCoin = coinUsed[x];
+            System.out.print(thisCoin + ", ");
+        }
+        System.out.print("]");
+    }
+    
 }
