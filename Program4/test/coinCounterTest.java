1085c728edfd30e9f1c6b714cb2f04ee1e1fe3d1
Jake O'Malley <jdomalley7@gmail.com>
2018-04-21 11:49:34 -0600

    Test Case Update
    updated test cases

--- Program4/test/coinCounterTest.java
+++ Program4/test/coinCounterTest.java
@@ -4,48 +4,20 @@
  * and open the template in the editor.
  */
 
-
-import static org.junit.Assert.*;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
- * @author rtill
+ * @author O'MalleyLaptop
  */
 public class coinCounterTest {
     
-    @Test
-    public void testAscendingOrder() {
-        System.out.println("Test 4");
-        int[] currency = {1,5,10,25};
-        int[] returns= {25,10,5,1};
-        assertArrayEquals(returns, coinCounter.coinCounter(currency, 41));
+    public coinCounterTest() {
     }
-    
-    @Test(expected = IllegalArgumentException.class)
-    public void testEmptyArray() {
-        System.out.println("Test 1");
-        int[] currency = {};
-        int[] returns= {25,10,5,1};
-        assertArrayEquals(returns, coinCounter.coinCounter(currency, 41));
+
+    @org.junit.Test
+    public void testSomeMethod() {
     }
-    
-    
-    @Test
-    public void testDecendingOrder() {
-        System.out.println("Test 2");
-        int[] currency = {25,10,5,1};
-        int[] returns= {25,10,5,1};
-        assertArrayEquals(returns, coinCounter.coinCounter(currency, 41));
-    }
-    
-    @Test
-    public void testMixedOrder() {
-        System.out.println("Test 3");
-        int[] currency = {5,25,1,10};
-        int[] returns= {25,10,5,1};
-        assertArrayEquals(returns, coinCounter.coinCounter(currency, 41));
-    }
-    
     
 }
